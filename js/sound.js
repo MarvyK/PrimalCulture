d1371dfeb2460314b373ed5a636750fac73f694e
MarvyK <marvin.poole@gmail.com>
2024-10-09 02:19:58 +0200

    Landing page creation
    Images, audio, layout, contact information added

---
+++ js/sound.js
@@ -0,0 +1,20 @@
+
+// let aud = document.getElementById("BubbleBobble"); 
+
+// function playAud() { 
+//   aud.play(); 
+// } 
+
+// function pauseAud() { 
+//   aud.pause(); 
+// } 
+var losAudio = document.getElementById("losAudio");
+var playPauseButton = document.getElementById("btn_playPause");
+
+function losAudio_playPause() {
+    var isPaused = losAudio.paused;
+    losAudio[isPaused ? "play" : "pause"]();
+    playPauseButton.innerHTML = '<i class="fa ' + (isPaused ? 'fa-music' : 'fa-pause') + '"></i>';
+}
+
+playPauseButton.addEventListener("click", losAudio_playPause);
