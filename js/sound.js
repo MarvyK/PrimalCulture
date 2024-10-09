5291b18e3d3b903b7bd0eea5ca5b369292e01967
MarvyK <marvin.poole@gmail.com>
2024-10-09 10:30:45 +0200

    Footer rework
    Wrapping and stacking. Samford details added

--- js/sound.js
+++ js/sound.js
@@ -14,7 +14,7 @@ var playPauseButton = document.getElementById("btn_playPause");
 function losAudio_playPause() {
     var isPaused = losAudio.paused;
     losAudio[isPaused ? "play" : "pause"]();
-    playPauseButton.innerHTML = '<i class="fa ' + (isPaused ? 'fa-music' : 'fa-pause') + '"></i>';
+    playPauseButton.innerHTML = '<i class="fa ' + (isPaused ? 'fa-music' : 'fa-pause') + ' bigger"></i>';
 }
 
 playPauseButton.addEventListener("click", losAudio_playPause);
